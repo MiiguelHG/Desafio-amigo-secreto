bdd12072017f22edca8697b5badbfdf9089c07c3
Miguel Hernandez <jesmiguelhdzg@gmail.com>
2025-08-05 12:58:35 -0600

    Sortear amigo

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-const amigo = document.getElementById('amigo');
-const error = document.getElementById('error');
 const nombreValido = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,}$/;
 
+const amigo = document.getElementById('amigo');
+const error = document.getElementById('error');
+const errorSorte = document.getElementById('errorSorte');
+const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
+
 function agregarAmigo() {
+    errorSorte.style.display = 'none';
+
     if (amigo.value.trim() === '') {
         error.innerHTML = 'Por favor, inserte un nombre.';
         error.style.display = 'block';
@@ -17,10 +23,32 @@ function agregarAmigo() {
     }
 
     limpiarCampo();
-
-    console.log(amigos)
 }
 
 function limpiarCampo() {
     amigo.value = '';
+}
+
+function listarAmigos() {
+    let contenido = '';
+
+    amigos.forEach((amigo) => {
+        contenido += `<li>${amigo}</li>`;
+    });
+
+    listaAmigos.innerHTML = contenido;
+}
+
+function generarAleatorio() {
+    return Math.floor(Math.random() * amigos.length)
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        errorSorte.innerHTML = 'No hay amigos en la lista';
+        errorSorte.style.display = 'block';
+    } else {
+        errorSorte.style.display = 'none';
+        resultado.innerHTML = amigos[generarAleatorio()];  
+    } 
 }
